dda9917c4316fe7ac3c87fe50d2a14b9e011a992
Neetalabba26 <83055786+Neetalabba26@users.noreply.github.com>
2021-06-21 13:56:16 +0530

    adding vpc, subnets, internet gateway and route table

---
+++ vpc.tf
@@ -0,0 +1,76 @@
+resource "aws_vpc" "test_vpc" {
+    cidr_block = "${var.vpc_cidr}"
+    tenancy = "default"
+    tags = {
+        Name = "test_vpc"
+    }
+}
+output "vpc_id" {
+  value = "${aws_vpc.test_vpc.id}"
+}
+
+
+resource "aws_internet_gateway" "igw"
+{
+  vpc_id = "${aws_vpc.test_vpc.id}"
+}
+
+/*     public_subnet     */
+
+resource "aws_subnet" "public_subnet"
+{
+  vpc_id = "${aws_vpc.test_vpc.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+/*     private_subnet_1     */
+
+resource "aws_subnet" "private_subnet_1"
+{
+  vpc_id = "${aws_vpc.test_vpc.id}"
+  cidr_block = "${var.private_subnet_1_cidr}"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet_1"
+  }
+} 
+
+/*     private_subnet_2     */
+
+resource "aws_subnet" "private_subnet_2"
+{
+  vpc_id = "${aws_vpc.test_vpc.id}"
+  cidr_block = "${var.private_subnet_2_cidr}"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private_subnet_2"
+  }
+} 
+
+/*     route table     */
+
+resource "aws_route_table" "RT" {
+    vpc_id = "${aws_vpc.test_vpc.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw.id}"
+    }
+
+    tags = {
+        Name = "Public Subnet 1 route table"
+    }
+}
+
+/*     route table association     */
+
+resource "aws_route_table_association" 'RT_association"
+{
+subnet_id = "${aws_subnet.pubblic_subnet.id}"
+route_table_id = "${aws_route_table.RT.id}"
+}
+
