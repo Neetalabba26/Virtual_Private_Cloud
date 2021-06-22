8bc22f2f6fd37c611f77c1601ef29ee83b21cece
Neetalabba26 <83055786+Neetalabba26@users.noreply.github.com>
2021-06-22 23:42:44 +0530

    private_subnet_1_id added

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,11 @@ output "vpc_id" {
 output "gateway_id" {
   value = "${aws_internet_gateway.igw.id}"
 }
-output "subnet_id" {
+output "public_subnet_id" {
   value = "${aws_subnet.public_subnet.id}"
+}
+output "private_subnet_1_id" {
+  value = "${aws_subnet.private_subnet_1.id}"
 }
 output "security_group_id" {
   value = "${aws_security_group.sg_1.id}"
