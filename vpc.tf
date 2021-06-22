f98139580be536e12b23d689e966ce84af0feb64
Neetalabba26 <83055786+Neetalabba26@users.noreply.github.com>
2021-06-22 19:03:03 +0530

    added ec2 and security group

--- vpc.tf
+++ vpc.tf
@@ -64,9 +64,57 @@ resource "aws_route_table" "RT" {
 
 /*     route table association     */
 
-resource "aws_route_table_association" 'RT_association"
+resource "aws_route_table_association" "RT_association"
 {
 subnet_id = "${aws_subnet.pubblic_subnet.id}"
 route_table_id = "${aws_route_table.RT.id}"
 }
+
+/*     ec2 instance     *
+
+resource "aws_instance" "web_server"
+{
+    ami = "${var.instance_ami}"
+    availability_zone = "us-east-1a"
+    instance_type = "t2.micro"
+    associate_public_ip_address = true
+    subnet_id = "${aws_subnet.pubblic_subnet.id}"
+    vpc_seurity_group_id = "${aws_security_group.sg_1.id}"
+    tags{
+        Name = "webserver"
+    }
+}
+
+/*     security group     */
+
+resource "aws_security_group" "sg_1" {
+  name        = "sg_1
+  vpc_id      = "${aws_vpc.test_vpc.id}"
+  
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.test_vpc.cidr_block]
+  }
+
+  ingress {
+      from_port      = 80
+      to_port        = 80
+      protocol       = "http"
+      cidr_blocks    = [aws_vpc.test_vpc.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_1"
+  }
+}
+    
 
